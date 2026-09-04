a176cadf6066a88aa104b18645dbe35cc9245bf2
Abi-mosiac <abigailtechpro@gmail.com>
2026-09-04 10:38:25 +0100

    Complete: Fizz Buzz Counter

--- challenges/main.py
+++ challenges/main.py
@@ -1,5 +1,13 @@
-def calculate_perimeter(length, width):
-    # TODO: store 2 * (length + width) in a variable called perimeter
-    perimeter = 2 * (length + width) # TODO: replace 0 with the correct calculation
-
-    return perimeter
+def fizzbuzz_counts(n):
+    counts = {"fizz": 0, "buzz": 0, "fizzbuzz": 0}
+    
+    for number in range(1, n + 1):
+        # TODO: check divisibility by 3 and 5, and increment the correct key in `counts`
+        if number % 3 == 0 and number % 5 == 0:
+            counts["fizzbuzz"] += 1
+        elif number % 3 == 0:
+            counts["fizz"] += 1
+        elif number % 5 == 0:
+            counts["buzz"] += 1
+    
+    return counts
