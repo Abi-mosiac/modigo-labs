f7a6910e6b9f6c26f2b7f228ca31327e91c7dee0
Abi-mosiac <abigailtechpro@gmail.com>
2026-09-09 14:03:48 +0100

    Complete: Write a function that compares two people's hobby sets and r

--- labs/main.py
+++ labs/main.py
@@ -1,6 +1,7 @@
-def count_unique_coordinates(coordinates):
-    unique = []
-    for coord in coordinates:
-        if coord not in unique:
-            unique.append(coord)
-    return len(unique)
+def compare_hobbies(person1_hobbies, person2_hobbies):
+    # TODO: use set operations to find shared, only_person1, and only_person2 hobbies
+    dictionary = {}
+    dictionary["shared"] = person1_hobbies.intersection(person2_hobbies)
+    dictionary["only_person1"] = person1_hobbies - person2_hobbies
+    dictionary["only_person2"] = person2_hobbies - person1_hobbies
+    return dictionary
